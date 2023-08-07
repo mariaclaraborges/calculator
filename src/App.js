f2e86579a3b8782e8bf64390b9a23ecd39bc35e0
Maria Clara Borges <mariaclarapiola@hotmail.com>
2023-08-07 10:40:20 -0300

    v01

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css"
+import { useState } from "react"
+
 
 function App() {
+  
+  const [num, setNum] = useState(0);
+  const [oldNum, setOldNum] = useState(0);
+  const [operation, setOperation] = useState(null);
+
+  function inputNum(e){
+    var input = e.target.value;
+    if(num === 0){
+      setNum(input);
+    }
+    else{
+      setNum(num + input);
+    }
+  }
+
+  function clear(e){
+    setNum(0);
+  }
+
+  function porcentage(){
+    if(num === 0){
+      return 0;
+    }
+    else{
+      setNum(num / 100);
+    }
+  }
+
+  function changeSignal(){
+      setNum(num * -1);
+  }
+
+  function operationHandler(e){
+    setOperation(e.target.value);
+    setOldNum(num); //armazena o valor atual
+    setNum(0); //zera o valor atual
+  }
+
+  function calculate(){
+    if (operation === "+"){
+      setNum(Number(oldNum) + Number(num));
+    }
+    if (operation === "*"){
+      setNum(Number(oldNum) * Number(num));
+    }
+    if (operation === "-"){
+      setNum(Number(oldNum) - Number(num));
+    }
+    if (operation === "/"){
+      setNum(Number(oldNum) / Number(num));
+    }
+
+    console.log("calculou");
+    console.log(oldNum)
+    console.log(operation)
+    console.log(num)    
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="result">{num}</div>
+      </div>
+      <button onClick={clear}>AC</button> {/*span-two ocupa 2 colunas*/}
+      <button onClick={changeSignal}>+/-</button>
+      <button onClick={porcentage}>%</button>
+      <button onClick={operationHandler} value={"/"}>/</button>
+      <button onClick={inputNum} value={1}>1</button>
+      <button onClick={inputNum} value={2}>2</button>
+      <button onClick={inputNum} value={3}>3</button>
+      <button onClick={operationHandler} value={"*"}>*</button>
+      <button onClick={inputNum} value={4}>4</button>
+      <button onClick={inputNum} value={5}>5</button>
+      <button onClick={inputNum} value={6}>6</button>
+      <button onClick={operationHandler} value={"+"}>+</button>
+      <button onClick={inputNum} value={7}>7</button>
+      <button onClick={inputNum} value={8}>8</button>
+      <button onClick={inputNum} value={9}>9</button>
+      <button onClick={operationHandler} value={"-"}>-</button>
+      <button onClick={inputNum} value={"."}>.</button>
+      <button onClick={inputNum} value={0}>0</button>
+      <button className="span-two" onClick={calculate}>=</button>
     </div>
-  );
+  )
 }
 
 export default App;
